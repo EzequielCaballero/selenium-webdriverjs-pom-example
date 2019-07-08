dd7abfb3bc3bbdd41efb6f9bb8081f983f027ce0
EzequielCaballero <ezequiel.caballero33@gmail.com>
2019-07-08 13:02:49 -0300

    Browsers configuration | chrome-firefox-ie

---
+++ src/config/config-data.js
@@ -0,0 +1,27 @@
+export const config = {
+    chrome: {
+        browserName: "chrome",
+        options: {
+            binary: "C:/Users/ezequ/AppData/Local/Google/Chrome/Application/chrome.exe",
+            profile: "--user-data-dir=C:/Users/ezequ/AppData/Local/Google/Chrome/User Data/Profile 1",
+            lang: "--lang=en",
+            windowSize: "--start-maximized",
+            infobars: "--disable-infobars"
+        }
+    },
+    firefox: {
+        browserName: "firefox",
+        options: {
+            binary: "C:/Program Files (x86)/Mozilla Firefox/firefox.exe",
+            profile: "C:/Users/ezequ/AppData/Roaming/Mozilla/Firefox/Profiles/h8bk6w6t.Test user",
+            lang: {
+                "intl.accept_languages": "en-US"
+            }
+        }
+    },
+    //It is better to config IE browser manually for testing purposes
+    iexplorer: {
+        browserName: "ie",
+        options: "none"
+    }
+}
