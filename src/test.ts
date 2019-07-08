f073dfb2daffedb34aba1722ac38106374662495
EzequielCaballero <ezequiel.caballero33@gmail.com>
2019-07-08 13:05:42 -0300

    Initiator file

---
+++ src/test.ts
@@ -0,0 +1,17 @@
+//DATA
+import { data } from "./data/test-data";
+//TEST
+import TestCase from "./test-cases/test-google-search";
+//Type of screenshot => desktop | browser | none
+let screenshotMethod: string = "desktop";
+
+/**
+ * BROWSERS SUPPORTED
+ * 'chrome'
+ * 'firefox'
+ * 'ie'
+ */
+
+//START
+let test: TestCase = new TestCase();
+test.start(screenshotMethod, "chrome", data);
